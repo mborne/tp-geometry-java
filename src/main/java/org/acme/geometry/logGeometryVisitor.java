08bec04c7659151e8da985d2d8527edb8437cf2c
Axel_Chassard_ensg <axel.chassard@ensg.eu>
2021-01-05 12:16:35 +0100

    declaration of classes

---
+++ src/main/java/org/acme/geometry/logGeometryVisitor.java
@@ -0,0 +1,13 @@
+package org.acme.geometry;
+
+public class logGeometryVisitor implements GeometryVisitor{
+	
+	public void visit(Point point) {
+		return;
+	}
+	
+	public void visit(LineString lineString) {
+		return;
+	}
+	
+}
