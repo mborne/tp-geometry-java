333e837702d252233fd15ec27c258d77849c0891
Paul Miancien <p.miancien@laposte.net>
2022-01-03 14:04:06 +0100

    0.1

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,30 @@
 package org.acme.geometry;
 
 public class Coordinate {
+	private double x = 0.0;
+	private double y = 0.0;
+	
+	
+	public Coordinate() {
+		super();
+	}
 
+
+	public Coordinate(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+
+	public double getX() {
+		return x;
+	}
+
+
+	public double getY() {
+		return y;
+	}
+	
+	
 }
