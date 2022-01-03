f99ad3ebfe5db3ddda6d1083550a1ceacedae02b
ljeannest <lucie.jeannest@orange.fr>
2022-01-03 13:59:53 +0100

    Création de la classe Coordinate

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,23 @@
 package org.acme.geometry;
 
 public class Coordinate {
-
+	public double x = 0.0;
+	public double y = 0.0;
+	
+	public Coordinate() {
+	}
+	
+	public Coordinate(double X, double Y) {
+		this.x=X;
+		this.y=Y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
 }
+
