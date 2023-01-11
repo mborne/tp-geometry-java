fa037eae8a645627d3d8995a47873611b3d938e7
formation <formation@ubuntu.ubuntu-domain>
2023-01-11 14:50:42 +0100

    test

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,22 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    double x;
+    double y;
 
+    public Coordinate(){
+
+    }
+    public Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -10,9 +10,17 @@ public class CoordinateTest {
 	@Test
 	public void testDefaultConstructor(){
 		// TODO
-		//Coordinate c = new Coordinate();
-		//Assert.assertEquals(0.0, c.getX(), EPSILON);
-		//Assert.assertEquals(0.0, c.getY(), EPSILON);
+		Coordinate c = new Coordinate();
+		Assert.assertEquals(0.0, c.getX(), EPSILON);
+		Assert.assertEquals(0.0, c.getY(), EPSILON);
 	}
+	@Test
+	public void testConstructorXY(){
+		// TODO
+		Coordinate c = new Coordinate(1.2,3.5);
+		Assert.assertEquals(1.2, c.getX(), EPSILON);
+		Assert.assertEquals(3.5, c.getY(), EPSILON);
+	}
+
 
 }
