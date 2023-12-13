9ece607df044b371d5c0ddefcb173b81a97ecd2d
VGiudicelli <vincent.giudicelli@ensg.eu>
2023-12-13 16:19:07 +0100

    remove setter for x and y

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -5,24 +5,16 @@ public class Coordinate {
 	private double x, y;
 	
 	public Coordinate(double x, double y) {
-		this.setX(x);
-		this.setY(y);
+		this.x = x;
+		this.y = y;
 	}
 
 	public double getX() {
 		return x;
 	}
 
-	public void setX(double x) {
-		this.x = x;
-	}
-
 	public double getY() {
 		return y;
-	}
-
-	public void setY(double y) {
-		this.y = y;
 	}
 
 }
